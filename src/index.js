4342194c5584721828d35dacabbed7db62c6ea5e
EunbiPark2 <parkeb555@gmail.com>
2021-08-04 16:29:46 +0900

    최종 완성 코드

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+//app 컴포넌트를 임포트하여 사용함
+//리액트는 <App />같은 표시를 컴포넌트로 인식 -> 반환 값을 화면에 그림
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
